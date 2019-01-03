2b032e69cae802499c15d6037c32362e84c4c5cd
Idan Amit <idan.amit@intl.att.com>
2019-01-03 18:18:37 +0200

    Fixed minor issues in sdc-pubusb
    Change-Id: I9d667fbb1740376e686824335f2dbae242f8d00c
    Issue-ID: SDC-2032
    Signed-off-by: Idan Amit <idan.amit@intl.att.com>

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { BasePubSub, IPubSubEvent, ISubscriber } from './lib/base-pubsub';
-import { PluginPubSub } from './lib/plugin-pubsub';
+import { BasePubSub, IPubSubEvent, ISubscriber } from './src/base-pubsub';
+import { PluginPubSub } from './src/plugin-pubsub';
 
 export { BasePubSub, PluginPubSub, IPubSubEvent, ISubscriber };
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "roots": [
-        "<rootDir>/lib"
+        "<rootDir>/src"
     ],
     "transform": {
         "^.+\\.ts?$": "ts-jest"
